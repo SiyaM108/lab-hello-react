172e014f157c9ec65352bc6e598c222ed0b408ff
SiyaM108 <deprominent@gmail.com>
2025-11-28 16:32:39 +0000

    change hero colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <section className="hero">
       <h1>Say hello to ReactJS</h1>
-      <p>You will learn how to use the most popular frontend library</p>
+      <p>You will learn how to use the most popular Frontend framework from scratch, to become a Ninka Developer</p>
       <button>Awesome!</button>
     </section>
   );
